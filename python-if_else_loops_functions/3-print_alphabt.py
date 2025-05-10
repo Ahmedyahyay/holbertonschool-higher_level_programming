79c93ff34050dc63462de7f6ff106abedb48bb37
Ahmedyahyay <ahmed@example.com>
2025-05-10 22:01:23 +0300

    add 3-print_alphabt file

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for alpha in range(97, 123):
-    if alpha not in [101, 113]:
-        print("{}".format(chr(alpha)), end="")
+
+for letter in range(97, 123):
+    if chr(letter) != 'e' and chr(letter) != 'q':
+        print("{}".format(chr(letter)), end="")
+

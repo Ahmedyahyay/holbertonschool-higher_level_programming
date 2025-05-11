58015ca9a170332885885c118feb96f17068a6f2
root <ahmeddawari@hotmail.com>
2025-05-11 16:04:48 +0000

    Implemented a program to print sorted names from the compiled module hidden_4.pyc excluding those starting with '__'

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
-    # Load the compiled Python module
-    from hidden_4 import *
+    import hidden_4
 
-    # Get the list of all names in the module
-    names = dir()
-
-    # Filter names that don't start with '__'
-    filtered_names = [name for name in names if not name.startswith("__")]
-
-    # Sort the filtered names
-    filtered_names.sort()
-
-    # Print each name on a new line
-    for name in filtered_names:
-        print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)

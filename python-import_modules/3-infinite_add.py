6de69478b3c683edf7a5cd5e0bacf3ef57606614
Ahmedyahyay <ahmed@example.com>
2025-05-11 15:10:00 +0300

    Implemented a program to sum all command-line arguments and print the result

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    # Initialize the sum variable
+    total = 0
+
+    # Iterate through the arguments (starting from index 1 to skip the script name)
+    for arg in sys.argv[1:]:
+        total += int(arg)
+
+    # Print the total sum
+    print(total)
+

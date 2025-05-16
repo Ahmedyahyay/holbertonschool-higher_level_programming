e0ea2e30f2f652ad37f2aa85ca3a05c703812d56
Ahmedyahyay <ahmed@example.com>
2025-05-16 16:09:31 +0300

    Add function to print a matrix of integers using str.format()

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(matrix)):
-            if i != len(matrix) - 1:
-                print('{:d}'.format(row[i]), end=" ")
-            else:
-                print('{:d}'.format(row[i]), end="")
-        print()
+        print(" ".join("{:d}".format(num) for num in row))
 

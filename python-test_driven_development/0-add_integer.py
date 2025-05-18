b23465dcaaed2e7df43a644ec9d154e51e189813
Ahmedyahyay <ahmed@example.com>
2025-05-18 17:54:35 +0300

    Implement add_integer function with type checks

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""
-Module that defines a function to add two integers.
-"""
+# 0-add_integer.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an integer addition function."""
+
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
+    """Return the integer addition of a and b.
 
-    Args:
-        a (int or float): first number
-        b (int or float, optional): second number, defaults to 98
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: if a or b is not an integer or float
-
-    Returns:
-        int: sum of a and b casted to integers
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    return (int(a) + int(b))

c5cc27aae552045eb7a484566da828319b2cfd96
Ahmedyahyay <ahmed@example.com>
2025-05-22 17:34:52 +0300

    Add an empty Square class definition

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""Square Class
-
-This class don't do anything for the moment
-
-"""
-
+"""Defines an empty class Square."""
 
 class Square:
+    """Represents a square."""
     pass

b0502a25c9c55c2505c0a326cc3c2d8383c377f3
Ahmedyahyay <ahmed@example.com>
2025-05-23 15:32:40 +0300

    Add Square class with input validation and private attribute

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    ''' A calss that defines a square'''
+
+    def __init__(self, size=0):
+        ''' Initializes the square withe an optional size'''
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size

c5d98f6cfc5fb32c80f4913d4c18457f44410948
Ahmedyahyay <ahmed@example.com>
2025-06-03 21:44:32 +0300

    add function to read and print contents of a UTF-8 text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+This module contains the function read_file(filename="")
+that reads a UTF-8 text file and prints its content to stdout.
+"""
+
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")

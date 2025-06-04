ee7d245a8ce1e6eb418b0f120dcdc9b5328f06fb
Ahmedyahyay <ahmed@example.com>
2025-06-05 01:36:26 +0300

    Add function to convert JSON string to python objec

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-This module contains an object represented by
-JSON string
-""""
+"""This module contains a function that returns a Python object
+ from a JSON string."""
+
+import json
+
 
 def from_json_string(my_str):
-"""Function returns an object represented by JSON string"""
-
+    """Converts a JSON string into a Python data structure."""
     return json.loads(my_str)

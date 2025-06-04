298d5a6f88853e81cfc3949c8342f37ff76143ee
Ahmedyahyay <ahmed@example.com>
2025-06-04 16:33:08 +0300

    Add function returns JSON

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""This module contains a function returns the JSON representation"""
+"""
+This module contains a function that returns
+the JSON representation of an object (as a string).
+"""
 
-
-impor json
+import json  # Used to convert Python objects to JSON strings
 
 
 def to_json_string(my_obj):
-    """Function returns JOSN representation"""
+    """
+    Converts a Python object to a JSON-formatted string.
 
+    Args:
+        my_obj (any): The Python object to be converted
+
+    Returns:
+        str: The JSON representation of the object as a string.
+    """
     return json.dumps(my_obj)

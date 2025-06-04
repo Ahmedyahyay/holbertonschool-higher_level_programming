bbcea835d92e449ee94e53d14643af6e96fa4881
Ahmedyahyay <ahmed@example.com>
2025-06-04 16:17:35 +0300

    Add function returns JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a function returns the JSON representation"""
+
+
+impor json
+
+
+def to_json_string(my_obj):
+    """Function returns JOSN representation"""
+
+    return json.dumps(my_obj)

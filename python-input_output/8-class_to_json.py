480be5d183884c0415f5f0cd33b0482d834d42e9
Ahmedyahyay <ahmed@example.com>
2025-06-07 13:59:25 +0300

    Returns the dictionary description of an object for JSON serialization

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""This module provides a function to convert a class instance into a JSON-serializable dictionary."""
+"""This module provides a function to convert a class
+ instance into a JSON-serializable dictionary."""
+
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization."""
+    """Returns the dictionary description of an object
+ for JSON serialization."""
     return obj.__dict__

27012362543b3af986b1ca59b403b50cc30615b0
Ahmedyahyay <ahmeddawari>
2025-06-28 11:41:28 +0300

    Add SQL script to create 'first_table' if it does not exist

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Create the table first_table with columns id and name
+-- If the table already exists, do not fail
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+

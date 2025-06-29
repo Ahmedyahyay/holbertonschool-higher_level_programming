1e4f3413bbf29fc5cc73a11fbe4c708f33eeb6e8
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-06-29 17:26:23 +0000

    Add file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with 'N' from the given MySQL database.
+"""Lists states"""
 
-Takes 3 arguments: mysql username, mysql password, and database name.
-Connects to MySQL on localhost:3306 using MySQLdb.
-Displays results sorted by states.id in ascending order.
-"""
-import sys
 import MySQLdb
-
-
-def main():
-    # Get arguments
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to MySQL
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password,
-                         db=db_name, charset="utf8")
-
-    cursor = db.cursor()
-
-    # Select states where name starts with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
-
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswith("N"):
+            print(row)
+    cur.close()
+    conn.close()

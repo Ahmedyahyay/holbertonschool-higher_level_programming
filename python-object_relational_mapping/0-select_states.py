2aba0ba43fc9fd0aff2404f8f6dd6174d3964551
Ahmedyahyay <ahmeddawari>
2025-06-29 19:36:17 +0300

    Fix: correct output format and query for task 0 - 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+    # Connect to MySQL database
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    # Create cursor to execute queries
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch and print all rows
+    for row in cur.fetchall():
         print(row)
+
+    # Close cursor and connection
     cur.close()
     conn.close()

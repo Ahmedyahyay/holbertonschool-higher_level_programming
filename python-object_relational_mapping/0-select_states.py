daf839882637fe8b53b21075b7c93f9bc71afd5b
Ahmed Dawwari <ahmeddawari@hotmail.com>
2025-06-29 17:00:10 +0000

    add

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,9 @@
-#!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa in ascending order by states.id
-"""
-import MySQLdb
-import sys
-
-if __name__ == "__main__":
-    # Connect to MySQL server
-    conn = MySQLdb.connect(
-        host="localhost", port=3306,
-        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
-    )
-
-    # Create a cursor
-    cur = conn.cursor()
-
-    # Execute the query
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch and print all rows
-    for row in cur.fetchall():
-        print(row)
-
-    # Clean up
-    cur.close()
-    conn.close()
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES
+("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
